e73814b2a410e07174e171b650eb83b101e731f7
AhmedBaid <baidahmed2005@gmail.com>
2026-01-04 09:15:59 +0100

    scalar

---
+++ scalar/src/main.rs
@@ -0,0 +1,20 @@
+use scalar::*;
+
+fn main() {
+    // sum
+    println!("sum: {}", sum(234, 2)); // 'sum: 236'
+
+    // diff
+    println!("diff: {}", diff(234, 2)); // 'diff: 232'
+
+    // product
+    println!("pro: {}", pro(23, 2)); // 'pro: 46'
+
+    // quotient
+    println!("quo: {}", quo(22.0, 2.0)); // 'quo: 11'
+    println!("quo: {}", quo(-128.23, 2.0)); // 'quo: -64.115'
+
+    // remainder
+    println!("rem: {}", rem(22.0, 2.0)); // 'rem: 0'
+    println!("rem: {}", rem(-128.23, 2.0)); // 'rem: -0.22999573'
+}
