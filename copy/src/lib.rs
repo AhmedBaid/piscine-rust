f594330c4cc7b63f0b05809e7c2e35b3aac088d9
AhmedBaid <baidahmed2005@gmail.com>
2026-01-05 16:29:30 +0100

    copy

--- copy/src/lib.rs
+++ copy/src/lib.rs
@@ -1,7 +1,25 @@
 pub fn nbr_function(c: i32) -> (i32, f64, f64) {
-    
+    (c, (c as f64).exp(), (c as f64).ln())
 }
 
-pub fn str_function(a: String) -> (String, String) {}
+pub fn str_function(a: String) -> (String, String) {
+    let mut res = String::new();
+    for rune in a.chars() {
+        if rune == ' ' {
+            res.push(rune)
+        } else {
+            let value = (rune.to_digit(10).unwrap_or(0) as f64).exp();
+            res.push_str(&value.to_string());
+        }
+    }
+    (a, res)
+}
 
-pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {}
+pub fn vec_function(b: Vec<i32>) -> (Vec<i32>, Vec<f64>) {
+    let mut res: Vec<f64> = Vec::new();
+    for ele in &b {
+        let f: f64 = (*ele as f64).ln();
+        res.push(f);
+    }
+    (b, res)
+}
