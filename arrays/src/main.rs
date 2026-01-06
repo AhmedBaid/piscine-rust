ce13a2c8e1a4e5dc5633fc847a958c5289573895
AhmedBaid <baidahmed2005@gmail.com>
2026-01-06 16:25:23 +0100

    arrays

---
+++ arrays/src/main.rs
@@ -0,0 +1,14 @@
+use arrays::*;
+
+fn main() {
+    let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let b = [5; 10];
+
+    println!("The sum of the elements in {:?} is {}", a, sum(&a));
+    println!("The sum of the elements in {:?} is {}", b, sum(&b));
+    println!(
+        "Array of {} elements filled with 10 = {:?}",
+        thirtytwo_tens().len(),
+        thirtytwo_tens()
+    );
+}
