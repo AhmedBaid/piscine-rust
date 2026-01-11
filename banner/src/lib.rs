029d6d1e561dbed00d7ae3d9a083bad88bc9d683
AhmedBaid <baidahmed2005@gmail.com>
2026-01-11 16:29:56 +0100

    banner

---
+++ banner/src/lib.rs
@@ -0,0 +1,68 @@
+use core::f64;
+use std::{collections::HashMap, num::ParseFloatError};
+
+pub struct Flag {
+    pub short_hand: String,
+    pub long_hand: String,
+    pub desc: String,
+}
+
+impl Flag {
+    pub fn opt_flag(name: &str, d: &str) -> Self {
+        Self {
+            short_hand: format!("-{}", name[0..1].to_string()),
+            long_hand: format!("--{}", name),
+            desc: d.to_string(),
+        }
+    }
+}
+
+pub type Callback = fn(&str, &str) -> Result<String, ParseFloatError>;
+
+pub struct FlagsHandler {
+    pub flags: HashMap<String, Callback>,
+}
+
+impl FlagsHandler {
+    pub fn add_flag(&mut self, flag: Flag, func: Callback) {
+        self.flags.insert(flag.long_hand, func);
+        self.flags.insert(flag.short_hand, func);
+    }
+
+    pub fn exec_func(&self, input: &str, argv: &[&str]) -> Result<String, String> {
+        let function = self.flags.get(input).unwrap();
+        let call = function(argv[0], argv[1]);
+        match call {
+            Ok(res) => Ok(res),
+            Err(e) => Err(e.to_string()),
+        }
+    }
+}
+
+pub fn div(a: &str, b: &str) -> Result<String, ParseFloatError> {
+    let var1: f64;
+    let var2: f64;
+    match a.parse::<f64>() {
+        Ok(num1) => var1 = num1,
+        Err(e) => return Err(e),
+    };
+    match b.parse::<f64>() {
+        Ok(num2) => var2 = num2,
+        Err(e) => return Err(e),
+    }
+    Ok((var1 / var2).to_string())
+}
+
+pub fn rem(a: &str, b: &str) -> Result<String, ParseFloatError> {
+    let var1: f64;
+    let var2: f64;
+    match a.parse::<f64>() {
+        Ok(num1) => var1 = num1,
+        Err(e) => return Err(e),
+    };
+    match b.parse::<f64>() {
+        Ok(num2) => var2 = num2,
+        Err(e) => return Err(e),
+    }
+    Ok((var1 % var2).to_string())
+}
