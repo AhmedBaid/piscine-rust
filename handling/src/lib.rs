f70e2b4aa84402f3587e1dbc2dbfb9b969d1ea33
AhmedBaid <baidahmed2005@gmail.com>
2026-01-11 13:48:53 +0100

    handling

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -2,5 +2,5 @@ use std::{fs::File, io::Write, path::Path};
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
     let mut file = File::create(path).unwrap();
-    write!(file, "{}", content).unwrap()
+    file.write_all(content.as_bytes()).unwrap()
 }
