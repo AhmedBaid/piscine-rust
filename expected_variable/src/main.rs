e5b0cfeef6e85ad1f78f21422e7fdc29e3b67166
AhmedBaid <baidahmed2005@gmail.com>
2026-01-12 18:43:28 +0100

    expected_variable

--- expected_variable/src/main.rs
+++ expected_variable/src/main.rs
@@ -1,3 +1,20 @@
+use expected_variable::*;
+
 fn main() {
-    println!("Hello, world!");
+    println!(
+        "{} close to it",
+        expected_variable("On_Point", "on_point").unwrap()
+    );
+    println!(
+        "{} close to it",
+        expected_variable("soClose", "so_close").unwrap()
+    );
+    println!(
+        "{:?}",
+        expected_variable("something", "something_completely_different").unwrap()
+    );
+    println!(
+        "{} close to it",
+        expected_variable("BenedictCumberbatch", "BeneficialCucumbersnatch").unwrap()
+    );
 }
