d53c7b0189245d0035f5d640634b1874d0d233ae
AhmedBaid <baidahmed2005@gmail.com>
2026-01-12 11:30:29 +0100

    middle_day

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,6 +1,6 @@
 use chrono::{Datelike, NaiveDate};
 pub fn middle_day(year: u32) -> Option<chrono::Weekday> {
-    if year / 4 == 0 && year / 100 == 0 && year / 400 == 0 {
+    if (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0) {
         return None;
     }
     let specefioed_date = NaiveDate::from_yo_opt(year as i32, 183).unwrap();
