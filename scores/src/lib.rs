8a657c43ea7b9fa14afa5181ddcd395d10245cc9
AhmedBaid <baidahmed2005@gmail.com>
2026-01-13 15:31:23 +0100

    scores

--- scores/src/lib.rs
+++ scores/src/lib.rs
@@ -3,18 +3,20 @@ use std::collections::HashMap;
 pub fn score(str: &str) -> u64 {
     let mut map = HashMap::new();
     let mut count = 0;
-    map.insert("aeioulnrst", 1);
-    map.insert("dg", 1);
-    map.insert("bdmp", 1);
-    map.insert("fhvmy", 1);
-    map.insert("k", 1);
-    map.insert("js", 1);
-    map.insert("qz", 1);
+    map.insert("aeioulnrst".to_string(), 1);
+    map.insert("dg".to_string(), 2);
+    map.insert("bdmp".to_string(), 3);
+    map.insert("fhvmy".to_string(), 4);
+    map.insert("k".to_string(), 5);
+    map.insert("js".to_string(), 8);
+    map.insert("qz".to_string(), 10);
 
-    for ch in str.chars() {
+    let word = str.to_lowercase();
+    for ch in word.chars() {
         for (key, value) in &map {
-            if key.contains(ch.to_ascii_lowercase()) {
+            if key.contains(ch) {
                 count += value;
+                break;
             }
         }
     }
