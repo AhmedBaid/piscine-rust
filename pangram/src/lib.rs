43a4a9b4aeee7951273d5173119cc753689f339d
AhmedBaid <baidahmed2005@gmail.com>
2026-01-13 16:09:32 +0100

    pangram

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,9 +1,10 @@
 pub fn is_pangram(s: &str) -> bool {
+    let str = s.to_lowercase();
     let all = "abcdefghijklmnopqrstuvwxyz";
-    for ch in all.chars(){
-        if !s.contains(ch){
-            return false
+    for ch in all.chars() {
+        if !str.contains(ch) {
+            return false;
         }
-    } 
+    }
     true
 }
