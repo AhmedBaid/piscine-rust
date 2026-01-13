b4be4829e2b8b6e820a8c3d18f8858e537b9e497
AhmedBaid <baidahmed2005@gmail.com>
2026-01-13 15:34:27 +0100

    scores

--- scores/src/lib.rs
+++ scores/src/lib.rs
@@ -5,10 +5,10 @@ pub fn score(str: &str) -> u64 {
     let mut count = 0;
     map.insert("aeioulnrst".to_string(), 1);
     map.insert("dg".to_string(), 2);
-    map.insert("bdmp".to_string(), 3);
-    map.insert("fhvmy".to_string(), 4);
+    map.insert("bcmp".to_string(), 3);
+    map.insert("fhvwy".to_string(), 4);
     map.insert("k".to_string(), 5);
-    map.insert("js".to_string(), 8);
+    map.insert("jx".to_string(), 8);
     map.insert("qz".to_string(), 10);
 
     let word = str.to_lowercase();
