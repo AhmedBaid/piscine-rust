694dbd6b7799c74836a72466e4450bf01b2d0525
AhmedBaid <baidahmed2005@gmail.com>
2026-01-13 15:35:16 +0100

    scores

--- scores/src/lib.rs
+++ scores/src/lib.rs
@@ -3,13 +3,13 @@ use std::collections::HashMap;
 pub fn score(str: &str) -> u64 {
     let mut map = HashMap::new();
     let mut count = 0;
-    map.insert("aeioulnrst".to_string(), 1);
-    map.insert("dg".to_string(), 2);
-    map.insert("bcmp".to_string(), 3);
-    map.insert("fhvwy".to_string(), 4);
-    map.insert("k".to_string(), 5);
-    map.insert("jx".to_string(), 8);
-    map.insert("qz".to_string(), 10);
+    map.insert("aeioulnrst", 1);
+    map.insert("dg", 2);
+    map.insert("bcmp", 3);
+    map.insert("fhvwy", 4);
+    map.insert("k", 5);
+    map.insert("jx", 8);
+    map.insert("qz", 10);
 
     let word = str.to_lowercase();
     for ch in word.chars() {
