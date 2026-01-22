a28cdf366a3a7d6a5dbc694b8d41a96bf8b88fb8
AhmedBaid <baidahmed2005@gmail.com>
2026-01-22 04:04:11 +0100

    commits

--- commits_stats/src/main.rs
+++ commits_stats/src/main.rs
@@ -1,7 +1,7 @@
 use commits_stats::{commits_per_week, commits_per_author};
 
 fn main() {
-	let contents = include_str!("commits.json");
+	let contents = include_str!("../commits.json");
 	let serialized = json::parse(&contents).unwrap();
 	println!("{:?}", commits_per_week(&serialized));
 	println!("{:?}", commits_per_author(&serialized));
